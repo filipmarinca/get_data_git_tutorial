2fb64900fc302143e3376fb940bfd4b863431a25
filipmarinca <filipmarinca@gmail.com>
2024-11-18 20:37:13 +0200

    main file added

---
+++ main.py
@@ -0,0 +1,10 @@
+"""app main"""
+from src.get_person_data import get_name_and_age
+
+try:
+    person = get_name_and_age(112)
+    print(
+        f"Name: {person['name']}, Age: {person['age']}"
+    )
+except KeyError:
+    print("Not a good user ID")
